9573a80f1055eb905e454a3d1336bbeed7f845cf
DuckDeck <3421902@qq.com>
2021-11-22 23:07:00 +0800

    搞定webview

--- lib/hotNews/newsWeb.dart
+++ lib/hotNews/newsWeb.dart
@@ -13,9 +13,12 @@ class NewsWebPage extends StatefulWidget {
 }
 
 class _NewsWebPageState extends State<NewsWebPage> {
+  late WebViewController webViewController;
+  late var title = widget.catInfo.name!;
   @override
   void initState() {
     super.initState();
+    print("加载的URL是${widget.catInfo.yutangUrl}");
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
@@ -23,13 +26,49 @@ class _NewsWebPageState extends State<NewsWebPage> {
 
 //发现一个严重的问题导致这个项目失败，这个网站的链接应该是加密的，然后应该是调用内部js解密，再生成url，目前这个问题无解。
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.catInfo.name!),
-      ),
-      body: WebView(
-        initialUrl: widget.catInfo.yutangUrl,
-      ),
-    );
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: WebView(
+            //真坑，这个默认不让用js导致这个网站打不开
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (con) {
+              webViewController = con;
+            },
+            onPageStarted: (start) {
+              print(start);
+            },
+            onPageFinished: (finish) {
+              print(finish);
+
+              webViewController.getTitle().then((value) => {
+                    if (value != null)
+                      {
+                        setState(() {
+                          title = value;
+                        })
+                      }
+                  });
+            },
+            onProgress: (index) {
+              print("WebView加载$index");
+            },
+            onWebResourceError: (error) {
+              print(error.description);
+            },
+            initialUrl: widget.catInfo.yutangUrl,
+          ),
+        ),
+        onWillPop: () async {
+          webViewController.canGoBack().then((value) => {
+                if (value)
+                  {webViewController.goBack()}
+                else
+                  {Navigator.of(context).pop()}
+              });
+          return false;
+        });
   }
 }
