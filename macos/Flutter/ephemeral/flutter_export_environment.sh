b9e1830713131e4f04b7df44aa6fbcda2f51301b
DuckDeck <3421902@qq.com>
2022-09-25 13:27:20 +0800

    更新flutter

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/ShadowEdge/Desktop/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ShadowEdge/Desktop/Project/flutter_work"
+export "FLUTTER_ROOT=/Users/shadowedge/Desktop/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/shadowedge/Desktop/Git/flutter_work"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/shadowedge/Desktop/Git/flutter_work/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/shadowedge/Desktop/Git/flutter_work/.dart_tool/package_config.json"
